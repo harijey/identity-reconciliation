dea10ff4d31fe39b6882c1025b4deb4765aa1c97
Hari Haran J <hari.j@gdn-commerce.com>
2023-07-10 14:20:21 +0530

    docker changes

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,4 @@
-FROM maven:3.8.6 as build
-WORKDIR /app
-COPY pom.xml .
-COPY src /src
-RUN mvn clean install
-
-FROM maven:3.8.6
-COPY --from=build /app/target/identity-reconciliation-0.0.1-SNAPSHOT.jar /usr/app/identity-reconciliation-0.0.1-SNAPSHOT.jar
-EXPOSE 9876
-ENTRYPOINT ["java","-jar","/usr/app/identity-reconciliation-0.0.1-SNAPSHOT.jar"]
+FROM adoptopenjdk:11-jre-hotspot
+ARG JAR_FILE=*.jar
+COPY target/${JAR_FILE} application.jar
+ENTRYPOINT ["java", "-jar", "application.jar"]
--- src/main/java/com/example/IdentityReconciliation/IdentityReconciliationApplication.java
+++ src/main/java/com/example/IdentityReconciliation/IdentityReconciliationApplication.java
@@ -2,10 +2,12 @@ package com.example.IdentityReconciliation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@PropertySource(value = "classpath:application.properties")
 public class IdentityReconciliationApplication {
 
 	public static void main(String[] args) {
